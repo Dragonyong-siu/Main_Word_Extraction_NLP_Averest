dcf649517229c89caa70b51d221cad92bd9a39a7
Dragonyong-siu <68500343+Dragonyong-siu@users.noreply.github.com>
2020-08-05 06:02:45 +0900

    Update K2_ FinderModel2_ver2.py

--- K2_ FinderModel2_ver2.py
+++ K2_ FinderModel2_ver2.py
@@ -22,15 +22,12 @@ class K2_FinderModel2(torch.utils.data.Dataset):
 # soft max를 거친 attention_weights 행렬얻기
       Attn_weights = self.attn(self.input_ids, self.token_type_ids, self.attention_mask)
       Ch_Attn_weights = Attn_weights[7].squeeze(0)[7]
-      #print(Ch_Attn_weights)
 # 위에서 얻은 attention_weights는 max_len = 96에 맞춰진 96 *96 행렬이므로 
 # 단어 추출을 위해서는 [CLS], [SEP]까지 고려한 TOKEN의 개수로 행렬을 뽑아야한다.
 # ----- [CLS]와 [SEP] 는 제외하기
       
       input_ex = self.df[idx]
       A = len(tokenizer(input_ex)['input_ids'])
-      #tokenizer(input_ex)['input_ids'][1:A-1]
-
       Ch_Attn_weights = Ch_Attn_weights[1:A-1, 1:A-1]
       
       STD = torch.std(Ch_Attn_weights, -1)
